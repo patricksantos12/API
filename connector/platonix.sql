f931ab851d667b1ce9a9ccf8c81c01a516bbb5df
imm.industry05 <imm.industry05@gmail.com>
2023-04-21 17:16:46 +0800

    initial commit for api connector platonix

---
+++ connector/platonix.sql
@@ -0,0 +1,9 @@
+create table platonixApp (
+    platonixID int,
+    plateNumber varchar(255),
+    carRegistrationStatus varchar(255),
+    carColor varchar(255),
+    carMaker varchar(255),
+    carModel varchar(255),
+    carCityLocation varchar(255)
+);
