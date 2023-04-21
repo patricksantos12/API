bea1a2001166bf63302b684f71977237251c77e0
patricksantos12 <patricksantos2627@gmail.com>
2023-04-22 03:26:17 +0800

    text

--- frontend/PlatonixLR/screens/TextEntry.js
+++ frontend/PlatonixLR/screens/TextEntry.js
@@ -1,85 +1,99 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-
-// formik
-import {Formik} from 'formik';
-
-// icons
+import { Formik } from 'formik';
 import { Octicons, Ionicons, Fontisto } from '@expo/vector-icons';
-
-
 import {
-    StyledContainer,
-    InnerContainer,
-    PageLogo,
-    PageTitle,
-    SubTitle,
-    StyledFormArea,
-    LeftIcon,
-    StyledInputLabel,
-    StyledTextInput,
-    RightIcon,
-    StyledButton,
-    ButtonText,
-    PicturesLogo,
-    Colors,
-    MsgBox,
-    Line,
-    ExtraView,
-    ExtraText,
-    TextLink,
-    TextLinkContent,
-    HelpLogo,
-    PageTitle1,
-    PageTitle2,
-    PageTitle3,
-
+  StyledContainer,
+  InnerContainer,
+  PageLogo,
+  PageTitle,
+  SubTitle,
+  StyledFormArea,
+  LeftIcon,
+  StyledInputLabel,
+  StyledTextInput,
+  RightIcon,
+  StyledButton,
+  ButtonText,
+  PicturesLogo,
+  Colors,
+  MsgBox,
+  Line,
+  ExtraView,
+  ExtraText,
+  TextLink,
+  TextLinkContent,
+  HelpLogo,
+  PageTitle1,
+  PageTitle2,
+  PageTitle3,
 } from './../components/styles';
 
-import {View} from 'react-native';
-
-// colors
-const {brand, darkLight, primary} = Colors;
-
-// keyboard avoiding view
 import KeyboardAvoidingWrapper from '../components/KeyboardAvoidingWrapper';
 
 const TextEntry = () => {
+  const [hidePassword, setHidePassword] = useState(true);
 
-    const [hidePassword, setHidePassword] = useState(true);
+  return (
+    <KeyboardAvoidingWrapper>
+      <StyledContainer>
+        <StatusBar style="dark" />
+        <InnerContainer>
+          <PageTitle3>Text-Entry Processing</PageTitle3>
+          <Line />
+          <SubTitle>Here</SubTitle>
+          <Line />
 
-    return (
-        <KeyboardAvoidingWrapper><StyledContainer>
-            <StatusBar style="dark" />
-            <InnerContainer>
-           
-                <PageTitle3>Text-Entry Processing</PageTitle3>
-                <Line />
-                <SubTitle>Here</SubTitle>
-                <Line />
+          <Formik
+            initialValues={{ serialnumber: '', password: '', enterName: '' }}
+            onSubmit={(values) => {
+              console.log(values);
+            }}
+          >
+            {({ handleChange, handleBlur, handleSubmit, values }) => (
+              <StyledFormArea>
+                <StyledTextInput
+                  label="Confirm Password"
+                  icon="number"
+                  placeholder="Please enter the texts here"
+                  placeholderTextColor={Colors.darkLight}
+                  onChangeText={handleChange('confirmPassword')}
+                  onBlur={handleBlur('confirmPassword')}
+                  value={values.confirmPassword}
+                  secureTextEntry={hidePassword}
+                  isPassword={true}
+                  hidePassword={hidePassword}
+                  setHidePassword={setHidePassword}
+                />
 
-                <Formik
-                    initialValues={{serialnumber: '', password: '', enterName: ''}}
-                    onSubmit={(values) => {
-                        console.log(values);
-                    }}
-                >{({handleChange, handleBlur, handleSubmit, values}) => (<StyledFormArea>
-                    
-                    
-                    
-                   
-                    <StyledButton onPress={handleSubmit}>
-                        <ButtonText>Enter</ButtonText>
-                    </StyledButton>
-
-                </StyledFormArea>
-                )}
-                </Formik>
-            </InnerContainer>
-        </StyledContainer>
-        </KeyboardAvoidingWrapper>
-       
-    );
+                <StyledButton onPress={handleSubmit}>
+                  <ButtonText>Enter</ButtonText>
+                </StyledButton>
+              </StyledFormArea>
+            )}
+          </Formik>
+        </InnerContainer>
+      </StyledContainer>
+    </KeyboardAvoidingWrapper>
+  );
 };
+const MyTextInput = ({label, icon, isPassword, hidePassword, setHidePassword, ...props}) => {
+    return(
+        <View>
+            <LeftIcon>
+                <Octicons name={icon} size={30} color={brand} />
+            </LeftIcon>
+            <StyledInputLabel>{label}</StyledInputLabel>
+            <StyledTextInput {...props}/>
+            {isPassword && (
+                <RightIcon onPress={() => setHidePassword(!hidePassword)}>
+                    <Ionicons name={hidePassword ? 'md-eye-off' : 'md-eye'} size={30} colors={darkLight}/>
+                </RightIcon>
+            )}
+        </View>
+    )
+
+}
+
 
 export default TextEntry;
