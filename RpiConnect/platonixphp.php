04e53d662efbc540ad8a01e5c2128a6dc1cc852c
Albert <atongoljr24@gmail.com>
2023-04-21 15:33:09 +0800

    sheesh

--- RpiConnect/platonixphp.php
+++ RpiConnect/platonixphp.php
@@ -1,6 +1,6 @@
 <?php
 if($_SERVER['REQUEST_METHOD'] = 'POST'){
-	$output = shell_exec('python /home/cisco/Desktop/PLATONIXSYSTEM/pLaTOnIXapi.py');
+	$output = shell_exec('python ../PLATONIXSYSTEM/pLaTOnIXapi.py');
 	echo $output;
 }else{
 	http_response_code(400);
